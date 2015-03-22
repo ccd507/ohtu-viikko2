b2cd7aca33a1ccaeabf02c1a49412360ed942251
ccd507 <ccd507@users.noreply.github.com>
2015-03-22 19:10:23 +0200

    9-injektointi-osa4

---
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/IKirjanpito.java
@@ -0,0 +1,8 @@
+package ohtu.verkkokauppa;
+
+import java.util.ArrayList;
+
+public interface IKirjanpito {
+    public void lisaaTapahtuma(String tapahtuma);
+    public ArrayList<String> getTapahtumat();
+}
--- Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
@@ -2,22 +2,12 @@ package ohtu.verkkokauppa;
 
 public class Viitegeneraattori implements IViitegeneraattori {
 
-    private static Viitegeneraattori instanssi;
-
-    public static Viitegeneraattori getInstance() {
-        if (instanssi == null) {
-            instanssi = new Viitegeneraattori();
-        }
-
-        return instanssi;
-    }
-    
     private int seuraava;
-    
-    private Viitegeneraattori(){
-        seuraava = 1;    
+
+    public Viitegeneraattori(){
+        seuraava = 1;
     }
-    
+
     @Override
     public int uusi(){
         return seuraava++;
